b7266ccb249749319f85669c3eda720ac14b738e
Zuygui <maxime@halbitte.fr>
2024-03-05 21:17:27 +0100

    feat: basic sfml window

--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
+
+#define SIZE 20
+#define SCREEN_WIDTH SIZE * SIZE
+#define SCREEN_HEIGHT SIZE * SIZE
+
 
 
 int main(int argc, char *argv[]) {
-    printf("Hello, world!\n");
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML Snake");
+
+    while (window.isOpen()) {
+        sf::Event e;
+        while (window.pollEvent(e)) {
+          if (e.type == sf::Event::Closed) window.close();
+        }
+
+        window.display();
+    }
     return 0;
 }
